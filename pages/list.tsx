ac1e5a8d6d7f39784c85fa820898033be7cb255e
maxicapodacqua <maxi.capodacqua@gartner.com>
2022-05-20 17:37:57 -0500

    Getting started

---
+++ pages/list.tsx
@@ -0,0 +1,11 @@
+import {NextPage} from "next";
+
+const List :NextPage = () => {
+    return (
+        <>
+            <h1>List of most wanted from FBI</h1>
+        </>
+    );
+};
+
+export default List;
