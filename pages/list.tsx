d8353d6f7faca74370f428b336b4876ebf7d1de5
maxicapodacqua <maxi.capodacqua@gartner.com>
2022-05-22 14:08:00 -0500

    Cleaning code

--- pages/list.tsx
+++ pages/list.tsx
@@ -1,36 +1,61 @@
 import {NextPage} from "next";
 import useSWR from 'swr';
 import fetcher from "../src/fetcher";
-import {LinearProgress} from "@mui/material";
+import {Alert, Card, CardMedia, LinearProgress} from "@mui/material";
 
+interface WantedImage {
+    original: string;
+    thumb: string;
+    large: string;
+    caption: string|null;
+}
 interface Wanted {
     aliases?: string[];
+    images?: WantedImage[];
 }
+
 interface WantedListResponse {
     total: number;
     items: Wanted[];
     page: number;
 }
 
-const List :NextPage = () => {
+const List: NextPage = () => {
 
-    const { data, isValidating, error } = useSWR<WantedListResponse>(process.env.NEXT_PUBLIC_API_FBI_URL + 'wanted/v1/list', fetcher);
+    const {
+        data,
+        isValidating,
+        error
+    } = useSWR<WantedListResponse>(process.env.NEXT_PUBLIC_API_FBI_URL + 'wanted/v1/list', fetcher);
     // console.log(data?.items.length);
     return (
         <>
             {isValidating ? <LinearProgress/> : null}
             <h1>List of most wanted from FBI</h1>
+            {error ? <Alert severity={'error'}>Something went wrong getting information from the FBI</Alert> : null}
             {data ?
-            <div>
-                <h2>Showing Page {data.page}</h2>
-                <ul>
+                <div>
+                    <h2>Showing Page {data.page}</h2>
                     {data.items.map((item: Wanted, index: number) => {
-                        return <li key={index}>{item.aliases?.join(', ') ?? 'No Name'}</li>
-                        }
-                    )}
-                </ul>
-            </div>
-             : null}
+                        return <Card>
+                            {item.images ?
+                                <CardMedia
+                                    component='img'
+                                    height='140'
+                                    image={item.images[0]!.thumb}
+                                    alt={item.images[0]!.caption ?? ''}
+                                /> : null}
+                        </Card>
+                    })}
+
+                    {/*<ul>*/}
+                    {/*    {data.items.map((item: Wanted, index: number) => {*/}
+                    {/*            return <li key={index}>{item.aliases?.join(', ') ?? 'No Name'}</li>*/}
+                    {/*        }*/}
+                    {/*    )}*/}
+                    {/*</ul>*/}
+                </div>
+                : null}
         </>
     );
 };
